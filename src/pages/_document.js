d93562d7898ec58738946a345514b1f46ee48ffb
Kingshuk3006 <kingsarkar3006@gmail.com>
2023-12-24 20:47:52 +0530

    hover effect added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600&display=swap" rel="stylesheet"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="icon"
+          href="/logo.png"
+          type="image.png"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
